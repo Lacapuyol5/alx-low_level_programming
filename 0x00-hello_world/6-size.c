0d1b7f81e88fa41b9aabb772a4c02edaa0a8261e
fab <fabiantoby80@gmail.com>
2022-09-09 03:24:41 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,6 @@ int b;
 long int c;
 long long int d;
 float e;
-
 printf("size of a char: %lu byte(s)\n", sizeof(a));
 printf("size of an int: %lu byte(s)\n", sizeof(b));
 printf("size of a long int: %lu byte(s)\n", sizeof(c));
