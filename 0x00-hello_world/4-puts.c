641dac492a0bbd7b8d02ba2e6d151e43b5a113f6
fab <fabiantoby80@gmail.com>
2022-09-08 21:35:21 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
+#include <stdio.h>
+
 /**
- * This is a  C program that prints a string
  *Description: main- Entry point
  *Return- Always 0(Success)
  */
-#include <stdio.h>
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle");
         return (0);
 }
