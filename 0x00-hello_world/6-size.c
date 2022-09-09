de3d2babfd5809b22837bdd9e0f254e71f3f58f3
fab <fabiantoby80@gmail.com>
2022-09-09 02:19:42 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,8 @@ char a;
 int b;
 long int c;
 long long int d;
-float e
+float e;
+
 printf("size of a char: %ld byte(s)\n", sizeof(a));
 printf("size of an int: %ld byte(s)\n", sizeof(b));
 printf("size of a long int: %ld byte(s)\n", sizeof(c));
