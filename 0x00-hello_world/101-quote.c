fc593eef65716625799d3aa59e2c7a75d58a11f8
fab <fabiantoby80@gmail.com>
2022-09-09 03:40:03 -0700

    help

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main -prints the content
- * Return: Always 0
+ * Return: returns 1
  */
 int main(void)
 {
-write("and that piece of art is useful\" - Dora Korpar,2015-10-19\n");
+write(2,"and that piece of art is useful\" - Dora Korpar,2015-10-19\n",59);
 return (1);	
 }
