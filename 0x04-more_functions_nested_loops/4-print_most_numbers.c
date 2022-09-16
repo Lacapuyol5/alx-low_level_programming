92c27eefac41e90de922528f84b994da2995aa5b
fab <fabiantoby80@gmail.com>
2022-09-15 17:55:30 -0700

    0

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_most_numbers - entry point
+* Description: Prints numbers 0 to 9 followed by a new line
+* Return: void
+*/
+void print_most_numbers(void)
+{
+int number;
+for (number = '0'; number <= '9'; number++)
+{
+if (number == '2' || number == '4')			;
+else
+_putchar(number);
+}
+_putchar('\n');
