d9fd2528699d922ec4bc5cea6bd73c1ade951a96
fab <fabiantoby80@gmail.com>
2022-09-27 09:05:54 -0700

    task2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strchr - Locates a character in a string
  * @s: The string to check
@@ -12,7 +11,7 @@ char *_strchr(char *s, char c)
 	unsigned int i = 0;
 	for (; *(s+i) != '\0'; i++)
 		if (*(s+i) == c)
-			return (s+1);
-	if (*(s+1) == c)
+			return (s+i);
+	if (*(s+i) == c)
 	return ('\0');
 }
