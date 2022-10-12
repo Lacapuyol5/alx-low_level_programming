5fb42ecc633e2805e814d8a1ffa504069477dbbc
fab <fabiantoby80@gmail.com>
2022-10-12 01:20:54 -0700

    all

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,4 @@
-ifndef DOG_H
+#ifndef DOG_H
 #define DOG_H
 
 /**
@@ -9,17 +9,17 @@ ifndef DOG_H
  *
  * Description: Longer description
  */
- struct dog
- {
-	 char *name;
-	 float age;
-	 char *owner;
- };
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
 
- /**
-  * dog_t - typedef for struct dog
-  */
- typedef struct dog dog_t;
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
